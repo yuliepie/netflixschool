776ca137794f90360af2ec657fe147427f920ce4
SeokKyunNa <engsk1211@gmail.com>
2021-10-08 01:24:35 +0900

    modify get_word_level_counts_for_content.py

--- server/scripts_etc/get_word_level_counts_for_content.py
+++ server/scripts_etc/get_word_level_counts_for_content.py
@@ -8,14 +8,10 @@ import numpy as np
 # df_word_level = pd.read_csv('C:/workspace/elice_web_project2/data_preprocessing/words/final_datasets/words_df_word_level.csv', index_col='Word')
 # df_lemmas = pd.read_csv('C:/workspace/elice_web_project2/data_preprocessing/words/final_datasets/existing_lemmas_final.csv', index_col='Word')
 
-# print(df_script)
-# print(df_word_level)
-# print(df_lemmas)
-
 def get_word_level_counts_for_content(title, df_script, df_word_level, df_lemmas):
-    df_script.set_index('word', inplace=True)
-    df_word_level.set_index('Word', inplace=True)
-    df_lemmas.set_index('Word', inplace=True)
+    # df_script.set_index('word', inplace=True)
+    # df_word_level.set_index('Word', inplace=True)
+    # df_lemmas.set_index('Word', inplace=True)
 
     #변형어 리스트 불러오기
     lemmas_dict = {}
@@ -54,23 +50,13 @@ def get_word_level_counts_for_content(title, df_script, df_word_level, df_lemmas
     df_script.index = df_script.index.map(convertToHeadForm)
     df_script_headed = df_script.groupby(df_script.index).sum()
     df_script_headed.sort_values('counts', ascending=False, inplace=True)
-    print(df_script_headed)
 
     counts_df_headed_joined = df_script_headed.join(df_word_level[['word_level']])
-    print(counts_df_headed_joined)
-
-    result = counts_df_headed_joined.groupby('word_level')
-    result = result.sum('counts')
-    result = result.transpose()
 
     result = counts_df_headed_joined.groupby('word_level').sum('counts').transpose()
     result['title'] = title
     result = result.set_index('title')
     result = result.rename(columns={1: 'level_1', 2: 'level_2', 3: 'level_3', 4: 'level_4', 5: 'level_5', 6: 'level_6'})
-
-    # df_result = pd.concat([df_result, result])
-    # df_result = df_result.sort_index()
-    result = result.sort_index()
 
     return result
 
