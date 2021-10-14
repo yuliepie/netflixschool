92c3d219dc3c9f540a17aec4d6561d3ea10f4993
hanstar07 <hanstar07@gmail.com>
2021-10-14 14:55:14 +0900

    modify Test Page

--- client/src/components/TestPage/TestQuestions/q_data.js
+++ client/src/components/TestPage/TestQuestions/q_data.js
@@ -16,84 +16,155 @@
 // ```
 
 export const QuestionData = {
-  1 :{
+  1: {
     id: 'int',
     question: '1',
     type: 'int',
     imgPath: '/img/Disenchantment_statue.png',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:'status'}, {choice:'standard'}, {choice:'statue'}, {choice:'statute'}, {choice:'stitch'}]
+    choices: [
+      { choice: 'status' },
+      { choice: 'standard' },
+      { choice: 'statue' },
+      { choice: 'statute' },
+      { choice: 'stitch' },
+    ],
+    answer: '3',
   },
-  2 :{
+  2: {
     id: 'int',
     question: '2',
     type: 'int',
     imgPath: '/img/GoodPlace_awful.png',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:'awful'}, {choice:'awesome'},	{choice:'fabulous'},	{choice:'brilliant'},	{choice:'terrific'}]
+    choices: [
+      { choice: 'awful' },
+      { choice: 'awesome' },
+      { choice: 'fabulous' },
+      { choice: 'brilliant' },
+      { choice: 'terrific' },
+    ],
+    answer: '3',
   },
-  3 :{
+  3: {
     id: 'int',
     question: '3',
     type: 'int',
     imgPath: '/img/Heroin(e)_judgment.png',
-    koreanSentence: '다음 중 빈칸에 들어갈 단어로 적절하지 않은 단어를 고르세요.',
-    choices: [{choice:'judgement'},	{choice:'discernment'},	{choice:'common sense'},	{choice:'chaos'},	{choice:'perception'}]
+    koreanSentence:
+      '다음 중 빈칸에 들어갈 단어로 적절하지 않은 단어를 고르세요.',
+    choices: [
+      { choice: 'judgement' },
+      { choice: 'discernment' },
+      { choice: 'common sense' },
+      { choice: 'chaos' },
+      { choice: 'perception' },
+    ],
+    answer: '3',
   },
-  4 :{
+  4: {
     id: 'int',
     question: '4',
     type: 'int',
     imgPath: '/img/FYRE_paid_off.png',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:"throw off"},	{choice:'flip off'},	{choice:'paid off '},	{choice:'lip off'},	{choice:'get off'}]
+    choices: [
+      { choice: 'throw off' },
+      { choice: 'flip off' },
+      { choice: 'paid off ' },
+      { choice: 'lip off' },
+      { choice: 'get off' },
+    ],
+    answer: '3',
   },
-  5 :{
+  5: {
     id: 'int',
     question: '5',
     type: 'int',
     imgPath: '/img/Suits_raiders.png',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:"reindeers"},	{choice:'announcer '}, {choice:'lawyer'},	{choice:'dealer'},	{choice:'raiders'}]
+    choices: [
+      { choice: 'reindeers' },
+      { choice: 'announcer ' },
+      { choice: 'lawyer' },
+      { choice: 'dealer' },
+      { choice: 'raiders' },
+    ],
+    answer: '3',
   },
-  6 :{
+  6: {
     id: 'int',
     question: '6',
     type: 'int',
     imgPath: '/img/Q_hopely_1.png',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:'ratio'}, {choice:'lavish'}, {choice:'later'}, {choice:'letter'}, {choice:'lethal'}]
+    choices: [
+      { choice: 'ratio' },
+      { choice: 'lavish' },
+      { choice: 'later' },
+      { choice: 'letter' },
+      { choice: 'lethal' },
+    ],
+    answer: '3',
   },
-  7 :{
+  7: {
     id: 'int',
     question: '7',
     type: 'int',
     imgPath: '/img/Qdadad2.jpg',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:'engaged'}, {choice:'encountered'},	{choice:'encouraged'},	{choice:'ensured'},	{choice:'enrolled'}]
+    choices: [
+      { choice: 'engaged' },
+      { choice: 'encountered' },
+      { choice: 'encouraged' },
+      { choice: 'ensured' },
+      { choice: 'enrolled' },
+    ],
+    answer: '3',
   },
-  8 :{
+  8: {
     id: 'int',
     question: '8',
     type: 'int',
     imgPath: '/img/Qasdad3.jpg',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:"what you have"},	{choice:'What happen to you'},	{choice:'What time it is'},	{choice:'where you belong'},	{choice:'How old you are'}]
+    choices: [
+      { choice: 'what you have' },
+      { choice: 'What happen to you' },
+      { choice: 'What time it is' },
+      { choice: 'where you belong' },
+      { choice: 'How old you are' },
+    ],
+    answer: '3',
   },
-  9 :{
+  9: {
     id: 'int',
     question: '9',
     type: 'int',
     imgPath: '/img/Qasdad4.jpg',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:"citizenship"},	{choice:'What happen to you'},	{choice:'civilization'},	{choice:'where you belong'},	{choice:'How old you are'}]
+    choices: [
+      { choice: 'citizenship' },
+      { choice: 'What happen to you' },
+      { choice: 'civilization' },
+      { choice: 'where you belong' },
+      { choice: 'How old you are' },
+    ],
+    answer: '3',
   },
-    10 :{
+  10: {
     id: 'int',
     question: '10',
     type: 'int',
     imgPath: '/img/Qasdad5.jpg',
     koreanSentence: '다음 중 빈칸에 들어갈 단어를 고르세요',
-    choices: [{choice:"dedicate ourselves"},	{choice:'are limited'}, {choice:'are admitted'},	{choice:'are subjected'},	{choice:'look forward'}]
-  }
+    choices: [
+      { choice: 'dedicate ourselves' },
+      { choice: 'are limited' },
+      { choice: 'are admitted' },
+      { choice: 'are subjected' },
+      { choice: 'look forward' },
+    ],
+    answer: '3',
+  },
 };
