a81d2b59a48f91f994e6a58036e3dfbd3bdc91a9
hanstar07 <hanstar07@gmail.com>
2021-10-15 01:08:35 +0900

    modify learning page v.1.0

--- client/src/components/Learning/Quizform.jsx
+++ client/src/components/Learning/Quizform.jsx
@@ -2,27 +2,99 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-const QuizWrapper = styled.div`
-  margin: 130px 0 0 0;
-  padding: 10px 0 100px 0;
-  background-color: gray;
-  width: 100%;
-`
+export default function Quizform({ file_path, question, choices, answer }) {
+  const [selectedAnswer, setSelectedAnswer] = useState('0');
+
+  const handleChange = (e) => {
+    setSelectedAnswer(e.target.value);
+  };
+
+  const checkAnswer = (e) => {
+    e.preventDefault();
+    console.log('selectedAnswer', selectedAnswer);
+    console.log('answer', answer);
+    return (
+      <div>
+        {selectedAnswer &&
+          (answer === selectedAnswer
+            ? alert('맞았습니다.')
+            : alert('틀렸습니다'))}
+      </div>
+    );
+  };
+  console.log('choices', choices);
+  console.log('answer', answer);
+
+  return (
+    <Container>
+      <QuestionWrapper>
+        <h3> Today's Quiz </h3>
+        <div>
+          <Image src={file_path} alt="questionimg" />
+          <Image2 src={file_path} alt="questionimg" />
+        </div>
+        <Subtitle>{question}</Subtitle>
+      </QuestionWrapper>
+      <Asking>{question}</Asking>
+      <ExampleWrapper>
+        {choices.map((answ, index) => {
+          return (
+            <Reply key={index}>
+              <Label
+                checked={selectedAnswer === String(index + 1) ? true : false}
+              >
+                <input
+                  type="radio"
+                  name="answer"
+                  value={index + 1}
+                  onChange={handleChange}
+                  checked={selectedAnswer === String(index + 1) ? true : false}
+                />
+                <span>{answ.choice}</span>
+              </Label>
+            </Reply>
+          );
+        })}
+        <Submit onClick={checkAnswer}>제출</Submit>
+      </ExampleWrapper>
+    </Container>
+  );
+}
 
 const Container = styled.div`
+  width: 1000px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: column;
-  padding: 50px 0;
-  font-size: 25px;
+`;
 
-`
+const QuestionWrapper = styled.div`
+  width: 90%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
+  background-color: black;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 2px gray;
+  position: relative;
+`;
 const Image = styled.img`
+  height: 400px;
+  width: 100%;
+  display: block;
+  margin: 0px auto;
+  border-radius: 30px 30px 0 0 / 30px 30px 0 0;
+`;
+
+const Image2 = styled.img`
   width: 1000px;
   height: 550px;
-  display: block; 
+  display: block;
   margin: 30px auto;
+  position: absolute;
+  top: 30%;
+  left: 30%;
+  display: none;
 `;
 
 const Asking = styled.p`
@@ -30,8 +102,15 @@ const Asking = styled.p`
   text-align: center;
 `;
 
-const Wrap = styled.div`
+const ExampleWrapper = styled.div`
   text-align: center;
+`;
+
+const Subtitle = styled.p`
+  font-size: 25px;
+  text-align: center;
+  color: #ffffff;
+  padding: 40px 0 40px 0;
 `;
 
 const Reply = styled.div`
@@ -43,73 +122,42 @@ const Reply = styled.div`
 const Submit = styled.button`
   margin-top: 20px;
 
-  font-size:30px;
+  font-size: 30px;
   background-color: #ff8906;
   border: 0px none;
-  border-radius : 10px;
+  border-radius: 10px;
   color: white;
   width: 9rem;
   height: 5rem;
 
-  :hover{
-      background-color: white;
-      cursor: pointer;
-      color: black;
+  :hover {
+    background-color: white;
+    cursor: pointer;
+    color: black;
   }
-`
+`;
 
-export default function Quizform (
-  {
-    file_path,
-    question,
-    choices,
-    answer
+const Label = styled.label`
+  cursor: pointer;
+  width: 13rem;
+  margin-top: 15px;
+  background-color: ${(props) => (props.checked ? '#000000' : '#ffffff')};
+  color: ${(props) => (props.checked ? '#ffffff' : '#000000')};
+  border-radius: 10px;
+  border: 2px solid #000000;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 23px;
+  padding: 20px 8px;
+  text-align: center;
+  text-decoration: none;
+
+  :hover {
+    background-color: #000000;
+    color: #ffffff;
   }
-  ) {
-  const [selectedAnswer, setSelectedAnswer] = useState("0");
 
-  const checkAnswer = (e) => {
-    e.preventDefault();
-    console.log('selectedAnswer', selectedAnswer)
-    console.log('answer', answer)
-    return (
-      <div>
-        { selectedAnswer && (answer === selectedAnswer ?
-          alert('맞았습니다.')
-        :
-          alert('틀렸습니다')
-        )}
-      </div>
-    )
+  > input {
+    display: none;
   }
-  console.log('choices', choices)
-  console.log('answer', answer)
-  return (
-
-    <QuizWrapper>
-      <Container>
-        <h3> Today's Quiz </h3>
-        <div><Image src={file_path} alt='questionimg' /></div>
-        <Asking>{question}</Asking>
-      </Container>
-      <Wrap>
-        {choices && choices.map((answ, index) => {
-          return (
-              <Reply key={index}>
-                <input
-                  type='radio'
-                  name='answer'
-                  value={`answ.choice`}
-                  onChange={(e) => {setSelectedAnswer(e.target.value)}}
-                />
-                <span>{answ.choice}</span>
-              </Reply>
-          )
-        })}
-        <Submit
-          onClick={checkAnswer}
-        >제출</Submit>
-      </Wrap>
-    </QuizWrapper>
-  )
-}
+`;
--- client/src/components/TestPage/TestPrecautions.js
+++ client/src/components/TestPage/TestPrecautions.js
@@ -3,97 +3,114 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import {Animator, Fade, ScrollContainer, ScrollPage, Sticky, batch, MoveOut} from 'react-scroll-motion';
+import {
+  Animator,
+  Fade,
+  ScrollContainer,
+  ScrollPage,
+  Sticky,
+  batch,
+  MoveOut,
+} from 'react-scroll-motion';
 import ScrollToBottom from '../common/ScrollBottom';
 
 const TestIntro = () => {
-    return (
-        <div style={{ marginLeft: '23rem', height:'20rem',width:'30rem',textAlign:'center'}}>
-            {/* <div style={{padding:'1rem'}}></div> */}
-            <span style={{ fontSize: "1.5rem", color: "black"}}>
-                정확한 수준에 맞게 공부하지 않는다면 100%의 효과를 볼 수 없습니다. 넷플릭스쿨의 테스트를 통해 당신의 현재 레벨을 확인해보세요! 
-            </span>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        marginLeft: '23rem',
+        height: '20rem',
+        width: '30rem',
+        textAlign: 'center',
+      }}
+    >
+      {/* <div style={{padding:'1rem'}}></div> */}
+      <span style={{ fontSize: '1.5rem', color: 'black' }}>
+        정확한 수준에 맞게 공부하지 않는다면 100%의 효과를 볼 수 없습니다.
+        넷플릭스쿨의 테스트를 통해 당신의 현재 레벨을 확인해보세요!
+      </span>
+    </div>
+  );
+};
 
 const Precautions = () => {
-    return(
-        <div style={{marginLeft: '23rem',width:'50rem'}}>
-            <span style={{ fontSize: "1rem", color: "black", textAlign:'center'}}>
-                <h1 style={{color:'#E82B0C', fontSize:'3rem'}}>
-                    <strong>주의사항</strong>
-                </h1>
-                <br />
-                <br />
-                <h2>
-                    테스트 도중 이탈할 시 테스트 내용은 저장되지 않습니다.<br />
-                    <br />
-                    다른 사람과 상의하거나 답을 찾아볼 경우 정확한 결과를 받을 수 없습니다.<br />
-                    <br />
-                </h2>
-                <br />
-                <p style={{fontSize:'70px'}}><strong>Ready?</strong></p>
-                <br />
-                <div>
-                    <Link to="/dotest">
-                        <YesBox>Yes</YesBox>
-                    </Link>
-                    <Link to="/">
-                        <YesBox>No</YesBox>
-                    </Link>
-                </div>
-            </span>
+  return (
+    <div style={{ marginLeft: '23rem', width: '50rem' }}>
+      <span style={{ fontSize: '1rem', color: 'black', textAlign: 'center' }}>
+        <h1 style={{ color: '#E82B0C', fontSize: '3rem' }}>
+          <strong>주의사항</strong>
+        </h1>
+        <br />
+        <br />
+        <h2>
+          테스트 도중 이탈할 시 테스트 내용은 저장되지 않습니다.
+          <br />
+          <br />
+          다른 사람과 상의하거나 답을 찾아볼 경우 정확한 결과를 받을 수
+          없습니다.
+          <br />
+          <br />
+        </h2>
+        <br />
+        <p style={{ fontSize: '70px' }}>
+          <strong>Ready?</strong>
+        </p>
+        <br />
+        <div>
+          <Link to="/dotest">
+            <YesBox>Yes</YesBox>
+          </Link>
+          <Link to="/">
+            <YesBox>No</YesBox>
+          </Link>
         </div>
-    )
-}
+      </span>
+    </div>
+  );
+};
 
 function TestPrecautions() {
-    return (
-            <div className="container">
-                <ScrollContainer>
-                    <ScrollPage page = {0}>
-                        <div className='testReadyFirst'>
-                            <h1>테스트를 통해 당신의 레벨을 확인해보세요!</h1>
-                        </div>
-                        <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -1000))}>
-                            <TestIntro />
-                        </Animator>
-                        <button onClick={ScrollToBottom}>
-                            xczc
-                        </button>
-                    </ScrollPage>
-                    <ScrollPage page={1}>
-                        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -50))}>
-                            <Precautions />
-                        </Animator>
-                    </ScrollPage>
-                </ScrollContainer>
-            </div>
-        );
-    }
+  return (
+    <div className="container">
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <div className="testReadyFirst">
+            <h1>테스트를 통해 당신의 레벨을 확인해보세요!</h1>
+          </div>
+          <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -1000))}>
+            <TestIntro />
+          </Animator>
+          <button onClick={ScrollToBottom}>xczc</button>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -50))}>
+            <Precautions />
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
+    </div>
+  );
+}
 
 export default TestPrecautions;
 
 const YesBox = styled.button`
-    margin-top: 20px;
-    font-size:60px;
-    background-color:black;
-    border-radius : 10px;
-    color: white;
-    width: 10rem;
-    height: 5rem;
-    margin-right:70px;
-    margin-left:70px;
+  margin-top: 20px;
+  font-size: 60px;
+  background-color: black;
+  border-radius: 10px;
+  color: white;
+  width: 10rem;
+  height: 5rem;
+  margin-right: 70px;
+  margin-left: 70px;
 
-    :hover{
-        background-color: #E82B0C;
-        cursor: pointer;
-        color: black;
-    }
+  :hover {
+    background-color: #e82b0c;
+    cursor: pointer;
+    color: black;
+  }
 `;
-
-
 
 // const TestPrecautions = () => {
 //     return(
@@ -127,15 +144,13 @@ const YesBox = styled.button`
 //                             <Link to="/">
 //                                 <YesBox>No</YesBox>
 //                             </Link>
-                        
+
 //                     </div>
 //                 </ReadyBox>
 //             </div>
 //         </IconContext.Provider>
 //     )
 // }
-
-
 
 // const IntroBlock = styled.div`
 //     display: flex;
@@ -164,9 +179,6 @@ const YesBox = styled.button`
 //         font-size: 70px;
 //     }
 // `;
-
-
-
 
 // const Box = styled.div`
 //     margin-left: 23rem;
