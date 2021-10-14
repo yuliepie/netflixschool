c1d89c60ada6b52132f9336bafca40ba4eb01519
[2기-2반] 이승환 <shakur25@naver.com>
2021-10-15 05:13:01 +0900

    content page again

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -30,9 +30,14 @@ const Wrapper2 = styled.div`
   justify-content: center;
   margin: 0 auto;
   align-items: center;
+
+  dt, dd {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
 `
 const Title = styled.div`
-  font-size: 1.5rem;
+  font-size: 35px;
   display: flex;
   flex-direction: column;
 `
@@ -90,9 +95,20 @@ const EngInfo = styled.div`
   font-size: 1.1rem;
   padding-bottom: 100px;
 
-  h3, h4 {
+  h3 {
+    font-size: 30px;
     margin-top: 10px;
     margin-bottom: 6px;
+  }
+
+  h4 {
+    font-size: 27px;
+    margin-top: 10px;
+    margin-bottom: 6px;
+  }
+
+  dt, dd {
+    font-size: 24px;
   }
 `
 
@@ -122,33 +138,33 @@ export default function Content ({location}) {
           <BackImage>
             <Wrapper2>
               <Title className='box_basic'>
-                <h2 className='title_name_en'>{content.title_en}</h2>
-                <h3 className='title_name_kr'>{content.title_kr}</h3>
+                <h2 className='title_name_en'>{content_detail.title_en}</h2>
+                <h3 className='title_name_kr'>{content_detail.title_kr}</h3>
               </Title>
               <ContentInfoWrapper>
                 <DetailInfo className='box_basic'>
-                  <div className="info_poster"><img src={content.img_path} alt='movie_poster' /></div>
+                  <div className="info_poster"><img src={content_detail.img_path} alt='movie_poster' /></div>
                   <div className='info_content'>
                   <ContentInfo className='inner_content'>
                     <dl className='list_content'>
                       <dt>개봉연도</dt>
-                      <dd>{content.release_year}</dd>
+                      <dd>{content_detail.release_year}</dd>
                     </dl>
                     <dl className='list_content'>
                       <dt>장르</dt>
-                      <dd>{content.genre}</dd>
+                      <dd>{content_detail.genre}</dd>
                     </dl>
                     <dl className='list_content'>
                       <dt>등급</dt>
-                      <dd>{isNaN(content.age_rating) ?   "전체연령가" : `${content.age_rating} 세 이용가`}</dd>
+                      <dd>{isNaN(content_detail.age_rating) ?   "전체연령가" : `${content_detail.age_rating} 세 이용가`}</dd>
                     </dl>
                     <dl className='list_content'>
                       <dt>감독</dt>
-                      <dd>{content.director}</dd>
+                      <dd>{content_detail.director}</dd>
                     </dl>
                     <dl className='list_content'>
                       <dt>러닝타임</dt>
-                      <dd>{content.running_time}</dd>
+                      <dd>{content_detail.running_time}</dd>
                     </dl>
                   </ContentInfo>
                   </div>
@@ -161,15 +177,15 @@ export default function Content ({location}) {
         <EngInfo>
           <dl className='list_content'>
             <h3>영화 내용</h3>
-            <dd>{content.story}</dd>
+            <dd>{content_detail.story}</dd>
           </dl>
           <div>
-            <h3>단어 난이도 : {content.word_difficulty_level}</h3>
-            <h3>WPS : {content.words_per_second}</h3>
-            <h3>종합 난이도 : {content.content_level} / 10</h3>
+            <h3>단어 난이도 : {content_detail.word_difficulty_level}</h3>
+            <h3>WPS : {content_detail.words_per_second}</h3>
+            <h3>종합 난이도 : {content_detail.content_level} / 10</h3>
             <h4>영화에 나오는 중요 단어들</h4>
-            {content.content_unique_words && <WordCloud 
-              words={content.content_unique_words}
+            {content_detail.content_unique_words && <WordCloud 
+              words={content_detail.content_unique_words}
             />}
             <h4>영화에 나오는 대표 단어 및 예문</h4>
             {content_detail && content_detail.example.map ((example) => {
