8a9dfb2db70a32a9c21b6f5b3b9deb5442758c24
[2기-2반] 이승환 <shakur25@naver.com>
2021-10-15 04:56:24 +0900

    modify content page

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -11,25 +11,61 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 30px;
+  /* padding-top: 30px; */
   background-color: #020202;
-  color: white;
-`
 
+  color: white;
+
+`
+const Wrapper1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 800px;
+`
+const Wrapper2 = styled.div`
+  padding-top: 30px;
+  min-width: 1000px;
+  max-width: 1000px;
+  justify-content: center;
+  margin: 0 auto;
+  align-items: center;
+`
 const Title = styled.div`
   font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+`
 
+const BackImage = styled.div`
+  background-image: url(${content_detail.img_path});
+  background-size: cover;
+  background-position:center center;
+  background-color: black;    
+
+  ::before {
+  content: '';
+  opacity: 0.7;
+  position: absolute;
+  top: 0px;
+  left: 368px;
+  right: 0px;
+  bottom: -44px;
+  background-color: #292929;
+}
 `
 
 const DetailInfo = styled.div`
 
 `
+
 const ContentInfoWrapper = styled.div`
   display: flex;
   max-width: 1000px;
   min-width: 1000px;
   font-size: 1.3rem;
-  height: 650px;
+  height: 630px;
+  padding-bottom: 100px;
 `
 
 const ContentInfo = styled.div`
@@ -45,12 +81,14 @@ const EngInfoWrapper = styled.div`
 `
 
 const EngInfo = styled.div`
-  margin-left: 130px;
+  max-width: 800px;
+  min-width: 800px;
   display: flex;
   flex-direction: column;
-  max-width: 1000px;
-  min-width: 1000px;
+  align-items: flex-start;
+  margin-left: 15%;
   font-size: 1.1rem;
+  padding-bottom: 100px;
 
   h3, h4 {
     margin-top: 10px;
@@ -80,39 +118,45 @@ export default function Content ({location}) {
 
   return (
     <Container>
-      <Title className='detail_title'>
-        <h2 className='title_name_en'>{content.title_en}</h2>
-        <h3 className='title_name_kr'>{content.title_kr}</h3>
-      </Title>
-      <ContentInfoWrapper>
-        <DetailInfo className='box_basic'>
-          <div className="info_poster"><img src={content.img_path} alt='movie_poster' /></div>
-          <div className='info_content'>
-          <ContentInfo className='inner_content'>
-            <dl className='list_content'>
-              <dt>개봉연도</dt>
-              <dd>{content.release_year}</dd>
-            </dl>
-            <dl className='list_content'>
-              <dt>장르</dt>
-              <dd>{content.genre}</dd>
-            </dl>
-            <dl className='list_content'>
-              <dt>등급</dt>
-              <dd>{isNaN(content.age_rating) ?   "전체연령가" : `${content.age_rating} 세 이용가`}</dd>
-            </dl>
-            <dl className='list_content'>
-              <dt>감독</dt>
-              <dd>{content.director}</dd>
-            </dl>
-            <dl className='list_content'>
-              <dt>러닝타임</dt>
-              <dd>{content.running_time}</dd>
-            </dl>
-          </ContentInfo>
-          </div>
-        </DetailInfo>
-      </ContentInfoWrapper>
+        <Wrapper1>
+          <BackImage>
+            <Wrapper2>
+              <Title className='box_basic'>
+                <h2 className='title_name_en'>{content_detail.title_en}</h2>
+                <h3 className='title_name_kr'>{content_detail.title_kr}</h3>
+              </Title>
+              <ContentInfoWrapper>
+                <DetailInfo className='box_basic'>
+                  <div className="info_poster"><img src={content_detail.img_path} alt='movie_poster' /></div>
+                  <div className='info_content'>
+                  <ContentInfo className='inner_content'>
+                    <dl className='list_content'>
+                      <dt>개봉연도</dt>
+                      <dd>{content_detail.release_year}</dd>
+                    </dl>
+                    <dl className='list_content'>
+                      <dt>장르</dt>
+                      <dd>{content_detail.genre}</dd>
+                    </dl>
+                    <dl className='list_content'>
+                      <dt>등급</dt>
+                      <dd>{isNaN(content_detail.age_rating) ?   "전체연령가" : `${content_detail.age_rating} 세 이용가`}</dd>
+                    </dl>
+                    <dl className='list_content'>
+                      <dt>감독</dt>
+                      <dd>{content_detail.director}</dd>
+                    </dl>
+                    <dl className='list_content'>
+                      <dt>러닝타임</dt>
+                      <dd>{content_detail.running_time}</dd>
+                    </dl>
+                  </ContentInfo>
+                  </div>
+                </DetailInfo>
+              </ContentInfoWrapper>
+            </Wrapper2>
+          </BackImage>
+        </Wrapper1>
       <EngInfoWrapper>
         <EngInfo>
           <dl className='list_content'>
@@ -123,8 +167,12 @@ export default function Content ({location}) {
             <h3>단어 난이도 : {content.word_difficulty_level}</h3>
             <h3>WPS : {content.words_per_second}</h3>
             <h3>종합 난이도 : {content.content_level} / 10</h3>
+            <h4>영화에 나오는 중요 단어들</h4>
+            {content.content_unique_words && <WordCloud 
+              words={content.content_unique_words}
+            />}
             <h4>영화에 나오는 대표 단어 및 예문</h4>
-            {content && content.example.map ((example) => {
+            {content_detail && content_detail.example.map ((example) => {
               return (
                 <div className='word_content' key={example.id}>
                   <dt>{example.word}</dt>
@@ -133,10 +181,6 @@ export default function Content ({location}) {
                 </div>
               )
             })}
-            <h4>영화에 나오는 중요 단어들</h4>
-            {content.content_unique_words && <WordCloud 
-              words={content.content_unique_words}
-            />}
           </div>
         </EngInfo>
       </EngInfoWrapper>
