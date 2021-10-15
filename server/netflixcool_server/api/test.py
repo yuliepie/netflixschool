88e4df4b9f72d793c4f43551853ddc4f0324d659
engsk <engsk1211@gmail.com>
2021-10-15 23:18:33 +0900

    backend: modify test api for word levels

--- server/netflixcool_server/api/test.py
+++ server/netflixcool_server/api/test.py
@@ -1,12 +1,32 @@
 from flask import request
-from flask_restx import Resource, Namespace, fields
+from flask_restx import Resource, Namespace, fields, reqparse
 from netflixcool_server.models import NetflixContent, TestQuestion
 import random
+from netflixcool_server.level_sets import get_level_sets
 
 test_ns = Namespace(name="test", description="영어 레벨 테스트 관련 api 리스트.")
 
-test_answers = test_ns.model("test_answers", {"answers": fields.List(fields.Integer)})
+content_fields = test_ns.model(
+    "content",
+    {
+        "id": fields.Integer,
+        "title_en": fields.String,
+        "title_kr": fields.String,
+        "img_path": fields.String,
+    }
+)
 
+test_answer_fields = test_ns.model(
+    "test_answer", 
+    {
+        "user_level": fields.String,
+        "normal_content": fields.List(fields.Nested(content_fields)),
+        "hard_content": fields.List(fields.Nested(content_fields))
+    }
+)
+
+json_parser = test_ns.parser()
+json_parser.add_argument('answers', type=list, location='json')
 
 def return_content_result(content):
     return {
@@ -23,31 +43,89 @@ class Test(Resource):
     """사용자에게 레벨 테스트 문제를 제공하고, 결과를 분석한 후 사용자의 영어 레벨과 추천 컨텐츠를 반환합니다."""
 
     @test_ns.doc("post_test_results")
-    @test_ns.expect(test_answers)
+    @test_ns.doc(parser=json_parser)
+    @test_ns.expect(json_parser)
+    @test_ns.marshal_with(test_answer_fields)
     def post(self):
-        data = request.get_json()
-        answers = data.get("answers")
 
-        # 정답 리스트로 알고리즘 연산 후 유저 레벨 도출
-        randoms = [random.randint(1, 5) for _ in range(100)]
-        result_level = random.choice(randoms)
+        MAX_LEVEL = 5
 
-        normal_contents = NetflixContent.query.filter_by(
-            content_level=result_level
+        args = json_parser.parse_args()
+
+        list_answers = args['answers']
+
+        current_level = 0
+        wrong_answers = 0
+        isStop = False
+        # 3개씩 나눠서 계산, 15번 문제까지 있다고 쳤을 때 range(1, 6)
+        for i in range(1, MAX_LEVEL+1):
+            list_temp = list_answers[(i*3)-3:i*3]   # {0:3}, {3:6}, ...
+            wrong_answers = 0
+            for answer in list_temp:
+                if answer==False:
+                    wrong_answers += 1
+                    if wrong_answers == 2:
+                        current_level = i
+                        isStop = True
+                        break
+            if isStop:
+                break
+        # 걸러진 레벨이 없다면 모든 레벨을 통과한 것이므로 최고 레벨로.
+        if current_level == 0:
+            current_level = MAX_LEVEL
+        
+        levelsets = get_level_sets()
+        minwordlevel = 1
+        maxwordlevel = 15
+
+        """ 현재 레벨 계산 """
+        for levelset in levelsets:
+            if current_level == levelset.level:
+                minwordlevel = min(levelset.word_levels)
+                maxwordlevel = max(levelset.word_levels)
+        
+        normal_contents = NetflixContent.query.filter(
+            NetflixContent.word_difficulty_level.between(minwordlevel, maxwordlevel)
         ).all()
-        normal_contents_details = [
-            return_content_result(x) for x in random.sample(normal_contents, 5)
-        ]
+        # 결과가 5개가 안 되면 그대로 출력
+        if len(normal_contents) < 5:
+            normal_contents_details = [
+                return_content_result(x) for x in normal_contents
+            ]
+        # 결과가 5개 이상이면 랜덤 5개 출력
+        else:
+            normal_contents_details = [
+                return_content_result(x) for x in random.sample(normal_contents, 5)
+            ]
+        
+        # 최고 레벨일 때 처리
+        if current_level == MAX_LEVEL:
+            next_level = current_level
+        else:
+            next_level = current_level + 1
+        
+        """ 다음 레벨 계산"""
+        for levelset in levelsets:
+            if next_level == levelset.level:
+                minwordlevel = min(levelset.word_levels)
+                maxwordlevel = max(levelset.word_levels)
 
-        hard_contents = NetflixContent.query.filter_by(
-            content_level=result_level + 1
+        hard_contents = NetflixContent.query.filter(
+            NetflixContent.word_difficulty_level.between(minwordlevel, maxwordlevel)
         ).all()
-        hard_contents_details = [
-            return_content_result(x) for x in random.sample(hard_contents, 5)
-        ]
+        # 결과가 5개가 안 되면 그대로 출력
+        if len(hard_contents) < 5:
+            hard_contents_details = [
+                return_content_result(x) for x in hard_contents
+            ]
+        # 결과가 5개 이상이면 랜덤 5개 출력
+        else:
+            hard_contents_details = [
+                return_content_result(x) for x in random.sample(hard_contents, 5)
+            ]
 
         result = {
-            "user_level": result_level,
+            "user_level": current_level,
             "normal_content": normal_contents_details,
             "hard_content": hard_contents_details,
         }
@@ -81,25 +159,35 @@ class TestQuestions(Resource):
     @test_ns.marshal_with(test_question_fields, as_list=True)
     def get(self):  
 
-        test_questions = TestQuestion.query.all()
+        levelsets = get_level_sets()
         questions = []
-        for question in test_questions:
-            questions.append(
-                {
-                    'id': question.id,
-                    'question': question.question,
-                    'type': question.type,
-                    'file_path': question.file_path,
-                    'level': question.level,
-                    'choice': [question.choice1, question.choice2, question.choice3, question.choice4, question.choice5],
-                    # 'choice1': question.choice1,
-                    # 'choice2': question.choice2,
-                    # 'choice3': question.choice3,
-                    # 'choice4': question.choice4,
-                    # 'choice5': question.choice5,
-                    'answer': question.answer,
-                }
-            )
+
+        # 각 레벨 별 문제들을 가져와서 랜덤 3개를 리스트에 넣음
+        for levelset in levelsets:
+            test_questions = TestQuestion.query.filter(
+                TestQuestion.level.between(min(levelset.word_levels), max(levelset.word_levels))
+            ).all()
+
+            random.shuffle(test_questions)
+            test_questions = test_questions[:3]
+
+            for question in test_questions:
+                questions.append(
+                    {
+                        'id': question.id,
+                        'question': question.question,
+                        'type': question.type,
+                        'file_path': question.file_path,
+                        'level': question.level,
+                        'choice': [question.choice1, question.choice2, question.choice3, question.choice4, question.choice5],
+                        # 'choice1': question.choice1,
+                        # 'choice2': question.choice2,
+                        # 'choice3': question.choice3,
+                        # 'choice4': question.choice4,
+                        # 'choice5': question.choice5,
+                        'answer': question.answer,
+                    }
+                )
 
         return questions
 
--- server/netflixcool_server/level_sets.py
+++ server/netflixcool_server/level_sets.py
@@ -1,15 +1,16 @@
 class LevelSet:
-    def __init__(self, label, rank, word_levels):
+    def __init__(self, label, rank, level, word_levels):
         self.label = (label,)
         self.rank = (rank,)
+        self.level = (level, )
         self.word_levels = word_levels
 
 
 def get_level_sets():
-    level_A = LevelSet("순수 한국인", "A", [1, 2, 3, 4, 5, 6, 7, 8])
-    level_B = LevelSet("영어 루키", "B", [9, 10])
-    level_C = LevelSet("영어 중수", "C", [11])
-    level_D = LevelSet("영어 고수", "D", [12])
-    level_E = LevelSet("영어 초고수", "E", [13, 14])
+    level_A = LevelSet("순수 한국인", "A", 1, [1, 2, 3, 4, 5, 6, 7, 8])
+    level_B = LevelSet("영어 루키", "B", 2, [9, 10])
+    level_C = LevelSet("영어 중수", "C", 3, [11])
+    level_D = LevelSet("영어 고수", "D", 4, [12])
+    level_E = LevelSet("영어 초고수", "E", 5, [13, 14])
 
     return [level_A, level_B, level_C, level_D, level_E]
